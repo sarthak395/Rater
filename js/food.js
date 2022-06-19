11d8bf5b4ff6aa6f0a667781217cfa9dd853fa3a
sarthak395 <sarthak@Sarthaks-MacBook-Air.local>
2022-06-19 05:40:00 +0530

    Uodate

--- js/food.js
+++ js/food.js
@@ -1,7 +1,11 @@
 console.log("hello")
 const form_=document.getElementById('Canteens/Mess');
+const contains=document.querySelectorAll('.con')
 
-function value()
-{
-    console.log("inval")
-}
+document.getElementById("subbtn").addEventListener("click",()=>{
+    console.log(form_.value)
+    const div=document.getElementById(form_.value)
+    contains.forEach(con=>con.style.display='none')
+    div.style.display='block'
+})
+
