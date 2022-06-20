826825d9438cca5c3753bb849008981ef2d8ce2a
sarthak395 <sarthak@Sarthaks-MacBook-Air.local>
2022-06-20 15:44:56 +0530

    mongo intro

--- index.js
+++ index.js
@@ -1,33 +1,61 @@
+//INCLUDING STUFF
 const express=require('express')
 const bodyParser=require('body-parser')
 const path=require('path')
 const app=express()
+const mongoose=require('mongoose');
 
+//CONNECTING TO MONGODB
+mongoose.connect('mongodb://localhost:27017/test')
+
+//Storing Course information
+const CourseSchema=new mongoose.Schema({
+    Year:Number,
+    Semester:String,
+    Programme:String,
+    Course_name:String
+});
+
+const course=mongoose.model('Course',CourseSchema) //collection is course
+
+// course.findOne({Year: 1, Semester: "Spring" , Programme: "CSE", Course_name: "Computer Programming" },function(err,findnewcourse){
+//     console.log(findnewcourse.Year)
+// })
+
+
+//PORT INFORMATION
 const port=82
 const server='127.0.0.1'
 
+//EXPRESS STUFF
 app.use(bodyParser.urlencoded())
-
 app.use(express.static(__dirname + '/'));
-
 app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'pug')
 
+//BACKEND ROUTES
 app.get('/',(req,res)=>{
-    res.status(200).sendFile(path.join(__dirname+'/views/main.html'))
+    res.status(200).render('main')
 })
-
 app.get('/electives',(req,res)=>{
-    res.status(200).sendFile(path.join(__dirname+'/views/electives.html'))
+    res.status(200).render('electives',{list:''})
 })
-
+app.post('/electives',(req,res)=>{
+    var data=req.body
+    course.find({Year:`${data.Year}`,Semester:`${data.Semester}`,Programme:`${data.Programme}`},function(err,courses){
+        res.status(200).render('electives',{list:courses})
+    })
+    
+})
 app.get('/food',(req,res)=>{
-    res.status(200).sendFile(path.join(__dirname+'/views/food.html'))
+    res.status(200).render('food')
 })
-
 app.get('/advisers',(req,res)=>{
     res.send("This is advice page")
 })
 
+
+//LISTENING TO PORT
 app.listen(port,()=>{
     console.log(`Successfully run at http://${server}:${port}`);
 })
--- js/electives.js
+++ js/electives.js
@@ -1,39 +1,39 @@
-var Year_var="1";
-var Sem_var="1";
-var Programme_var="1";
+var Year_var=1;
+var Sem_var="Spring";
+var Programme_var="CSE";
 
-let courses_year_sem_prog=[]
+// let courses_year_sem_prog=[]
 
-let courses = [
-    {Year: "1", Semester: "1" , Programme: "1", Course_name: "Computer Programming" },
-    {Year: "3", Semester: "2" , Programme: "1", Course_name: "Compilers"},
-    {Year: "4", Semester: "2" , Programme: "3", Course_name: "Computer Systems and Organisation"},
-    {Year: "2", Semester: "1" , Programme: "7", Course_name: "Introduction to Software Systems"},
-    {Year: "4", Semester: "2" , Programme: "3", Course_name: "Data Structures and Algorithms"},
-    {Year: "5", Semester: "2" , Programme: "2", Course_name: "Linear Algebra"}
-];
+// let courses = [
+//     {Year: 1, Semester: "Spring" , Programme: "CSE", Course_name: "Computer Programming" },
+//     {Year: 3, Semester: "Monsoon" , Programme: "CSE", Course_name: "Compilers"},
+//     {Year: 4, Semester: "Monsoon" , Programme: "CLD", Course_name: "Computer Systems and Organisation"},
+//     {Year: 2, Semester: "Spring" , Programme: "ECD", Course_name: "Introduction to Software Systems"},
+//     {Year: 4, Semester: "Monsoon" , Programme: "CLD", Course_name: "Data Structures and Algorithms"},
+//     {Year: 5, Semester: "Monsoon" , Programme: "CSD", Course_name: "Linear Algebra"}
+// ];
 
 function Year(event){
     Year_var= event.value;
-    courses_year_sem_prog=courses.filter(function(e){
-        return e.Year == Year_var && e.Semester == Sem_var && e.Programme == Programme_var
-    })
+    // courses_year_sem_prog=courses.filter(function(e){
+    //     return e.Year == Year_var && e.Semester == Sem_var && e.Programme == Programme_var
+    // })
     // console.log(Year_var)
 }
 
 function Semester(event){
     Sem_var= event.value;
-    courses_year_sem_prog=courses.filter(function(e){
-        return e.Year == Year_var && e.Semester == Sem_var && e.Programme == Programme_var
-    })
+    // courses_year_sem_prog=courses.filter(function(e){
+    //     return e.Year == Year_var && e.Semester == Sem_var && e.Programme == Programme_var
+    // })
     // console.log(Sem_var)
 }
 
 function Programme(event){
     Programme_var= event.value;
-    courses_year_sem_prog=courses.filter(function(e){
-        return e.Year == Year_var && e.Semester == Sem_var && e.Programme == Programme_var
-    })
+    // courses_year_sem_prog=courses.filter(function(e){
+    //     return e.Year == Year_var && e.Semester == Sem_var && e.Programme == Programme_var
+    // })
     // console.log(Programme_var)
 }
 
@@ -41,32 +41,60 @@ function Programme(event){
 
 function display()
 {
-    const ol=document.getElementById('list')
-    var length=courses_year_sem_prog.length
-    if(length==0)
-    {
-        ol.innerHTML='<p>No Such Courses</p>'
-        return
-    }
-    ol.innerHTML=""
-    for(var i=0;i<length;i++)
-    {
-        const li=document.createElement('li')
-        li.innerText=courses_year_sem_prog[i].Course_name
-        ol.append(li);
-    }
+    // const ol=document.getElementById('list')
+    // var length=courses_year_sem_prog.length
+    // if(length==0)
+    // {
+    //     ol.innerHTML='<p>No Such Courses</p>'
+    //     return
+    // }
+    // ol.innerHTML=""
+    // for(var i=0;i<length;i++)
+    // {
+    //     const li=document.createElement('li')
+    //     li.innerText=courses_year_sem_prog[i].Course_name
+    //     ol.append(li);
+    // }
+
+    const form=document.createElement('form')
+    form.setAttribute('action','/electives')
+    form.setAttribute('method','post')
+    // var str='{'+'"Year"'+':'+Year_var+','+'"Semester"'+':'+Sem_var+','+'"Programme"'+':'+Programme_var+'}'
+    // var str=JSON.stringify(`{"Year":${Year_var},"Semester":"${Sem_var}","Programme":"${Programme_var}"}`)
+    // for(var i=0;i<length;i++)
+    // {
+    //     str+=courses_year_sem_prog[i].Course_name+",";
+    // }
+    const input1=document.createElement('input')
+    input1.name="Year"
+    input1.value=Year_var
+    form.appendChild(input1)
+
+    const input2=document.createElement('input')
+    input2.name="Semester"
+    input2.value=Sem_var
+    form.appendChild(input2)
+
+    const input3=document.createElement('input')
+    input3.name="Programme"
+    input3.value=Programme_var
+    form.appendChild(input3)
+
+    document.getElementById('list').appendChild(form)
+    form.submit()
+    document.getElementById('list').removeChild(form)
 }
 document.getElementById("btn").addEventListener('click',display)
 
 
-function Initial_load()
-{
+// function Initial_load()
+// {
 
-    courses_year_sem_prog=courses.filter(function(e){
-        return e.Year == Year_var && e.Semester == Sem_var && e.Programme == Programme_var
-    })
-    // console.log("hi")
+//     courses_year_sem_prog=courses.filter(function(e){
+//         return e.Year == Year_var && e.Semester == Sem_var && e.Programme == Programme_var
+//     })
+//     // console.log("hi")
 
-}
+// }
 
-Initial_load()
+// Initial_load()
